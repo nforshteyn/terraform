276e64dea6eab6316aefb1b4563ae72c17695954
Nick FORSHTEYN <nick.forshteyn.e@thalesdigital.io>
2020-06-04 12:49:41 +1000

    Added Network security

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "azurerm" {
   # If you are using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
-  subscription_id = "${var.subscription_id}"
-  client_id = "${var.client_id}"
-  client_secret = "${var.client_secret}"
-  tenant_id = "${var.tenant_id}"
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
 
 variable subscription_id {}
